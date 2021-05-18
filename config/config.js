c71f72c076b0a8c4b9a09884ea86251b23acb3d7
Prakash Singh <40623711+DestinyCall@users.noreply.github.com>
2021-05-18 23:37:59 +0530

    Config Update

--- config/config.js
+++ config/config.js
@@ -1,7 +1,6 @@
 const env = process.env.NODE_ENV || "development";
 
 if (env === "development") {
-  process.env.PORT = 9001;
-  process.env.MONGODB_URI = "mongodb://localhost:27017/service-feedback-db";
+  require('dotenv').config();
 }
 console.log(`Server is running in ${env} mode!`);
