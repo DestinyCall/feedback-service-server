4dd7611c26d6dd2d5d391f08f1e4b2c5fedbc4ec
Prakash Singh <40623711+DestinyCall@users.noreply.github.com>
2021-05-22 08:02:09 +0530

    linter and prettier applied

--- service/auth/passport-google.js
+++ service/auth/passport-google.js
@@ -1,39 +1,41 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const User = require("../../models/Users");
 
-const User = require("../../models/Users")
-
-const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_CALLBACK_URL } = process.env;
+const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_CALLBACK_URL } =
+  process.env;
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
-  User.findById(id).then(user => {
+  User.findById(id).then((user) => {
     done(null, user);
   });
 });
 
-passport.use(new GoogleStrategy(
+passport.use(
+  new GoogleStrategy(
     {
       clientID: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
       callbackURL: GOOGLE_CALLBACK_URL,
-      proxy: true
+      proxy: true,
     },
-    async (accessToken, refreshToken, profile, done) => 
-    {
+    async (accessToken, refreshToken, profile, done) => {
       const existingUser = await User.findOne({ googleId: profile.id });
-
-      if (existingUser) 
-      {
+      if (existingUser) {
         return done(null, existingUser);
       }
-      const user = await new User({ googleId: profile.id, email:profile.emails[0].value, registeredOn: new Date().getTime()}).save();
+      const user = await new User({
+        googleId: profile.id,
+        email: profile.emails[0].value,
+        registeredOn: new Date().getTime(),
+      }).save();
       done(null, user);
     }
   )
-)
+);
 
 module.exports = passport;
